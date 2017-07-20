8a18c709df0697b0d59783a3f3448df0b34d8f0c
karankg1997 <karankg1997@gmail.com>
2017-07-20 17:32:59 +0530

    E-commerceProject

---
+++ backend/src/main/java/com/project/service/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+package com.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.project.dao.ProductDao;
+import com.project.model.Product;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements ProductService{
+	
+	@Autowired
+	private ProductDao productDao;
+
+	public List<Product> getAllProducts() {
+		return productDao.getAllProducts();
+	}
+
+	public Product getProductById(int productId) {
+		return productDao.getProductById(productId); 
+	}
+
+	public void addProduct(Product product) {
+		productDao.addProduct(product);
+		
+	}
+
+	public void updateProduct(Product product) {
+		productDao.updateProduct(product);
+		
+	}
+
+	public void deleteProduct(int productId) {
+		productDao.deleteProduct(productId);
+		
+	}
+
+	public List<Product> getAllProductsByCategory(String category) {
+		return productDao.getAllProductsByCategory(category);
+	}
+}
