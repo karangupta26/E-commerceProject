3dfd250bb9738a1d31167452da3dbe932845c5df
karankg1997 <karankg1997@gmail.com>
2017-07-22 23:21:15 +0530

    E-commerceProject

--- backend/src/main/java/com/project/model/Category.java
+++ backend/src/main/java/com/project/model/Category.java
@@ -1,13 +1,27 @@
 package com.project.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+ 
+@Entity
 public class Category {
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int cid;
 	private String idealfor;
 	private String sleeve;
 	private String type;
 	private String necktype;
 	private int pockets;
+	@OneToOne
+	@JoinColumn(name="pid")
+	private Product product;
+	
 	public int getPockets() {
 		return pockets;
 	}
@@ -43,6 +57,12 @@ public class Category {
 	}
 	public void setIdealfor(String idealfor) {
 		this.idealfor = idealfor;
+	}
+	public void setProduct(Product product){
+		this.product=product;
+	}
+	public Product getProduct(Product product){
+		return product;
 	}
 
 }
